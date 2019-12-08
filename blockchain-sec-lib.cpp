b89a618a524546d46fe17c895d93d82469e80a8e
Kale Yuzik (kay851) <kaleyuzik@gmail.com>
2019-12-08 00:05:19 -0600

    Entered correct JSON format/data for eth_sendTransaction. Improved readability of JSON strings.

--- blockchain-sec-lib.cpp
+++ blockchain-sec-lib.cpp
@@ -1,6 +1,7 @@
 #include "gason.h"
 
 #define IPC_READ_BUFFER_LENGTH		64
+#define ETH_DEFAULT_GAS				"0x7A120"
 
 using namespace std;
 using namespace blockchain-sec;
@@ -10,8 +11,9 @@ namespace blockchain-sec {
 class blockchain-sec::Blockchain-Sec-Lib {
 
 	public:
-		Blockchain-Sec-Lib(std::string const ipc_path, std::string const eth_sec_contract_addr) {
+		Blockchain-Sec-Lib(std::string const ipc_path, std::string eth_my_addr, std::string const eth_sec_contract_addr) {
 			this->ipc_path = ipc_path;
+			this->eth_my_addr = eth_my_addr; //TODO Check for reasonableness
 			this->eth_sec_contract_addr = eth_sec_contract_addr; //TODO Check for reasonableness
 		}
 
@@ -20,20 +22,36 @@ class blockchain-sec::Blockchain-Sec-Lib {
 		}
 
 		std::string eth_call(std::string abi_data) {
-			std::string json_request = "{""jsonrpc"":""2.0"",""method"":""eth_call"",""params"":[{""to"":""" + this->eth_sec_contract_addr + """,""gasPrice"":0,""data"":""" + abi_data + """}],""id"":1}";
+			std::string json_request = "{""jsonrpc"":""2.0"","
+										"""method"":""eth_call"","
+										"""params"":[{"
+											"""to"":""" + this->eth_sec_contract_addr + ""","
+											"""gasPrice"":0,"
+											"""data"":""" + abi_data +
+										"""}],""id"":1}";
 			cout << "eth_call()\n";
 			return this->eth_ipc_request(json_request);
 		}
 
 		std::string eth_sendTransaction(std::string abi_data) {
-			std::string json_request = "{""jsonrpc"":""2.0"",""method"":""eth_call"",""params"":[{""to"":""" + this->eth_sec_contract_addr + """,""gasPrice"":0,""data"":""" + abi_data + """}],""id"":1}";
+			std::string json_request = "{""jsonrpc"":""2.0"","
+										"""method"":""eth_sendTransaction"""
+										",""params"":[{"
+											"""from"":""" + this->eth_my_addr + ""","
+											"""to"":""" + this->eth_sec_contract_addr + ""","
+											"""gas"":0,"
+											"""gasPrice"":""" + ETH_DEFAULT_GAS + ""","
+											"""data"":""" + abi_data +
+										"""}],"
+										"""id"":1}";
 			cout << "eth_sendTransaction()\n";
 			return this->eth_ipc_request(json_request);
 		}
 
 	private:
-		std::string eth_sec_contract_addr;
 		std::string ipc_path;
+		std::string eth_my_addr;
+		std::string eth_sec_contract_addr;
 
 		std::string eth_ipc_request(std::string json_request) {
 			std::string json;
