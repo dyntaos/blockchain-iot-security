9b145edbc7de15ff94d99542561109765d5798cb
Kale Yuzik (kay851) <kaleyuzik@gmail.com>
2019-12-11 01:24:08 -0600

    Added test code for double() smart contract.

--- client.cpp
+++ client.cpp
@@ -10,11 +10,15 @@ BlockchainSecLib *sec;
 int main(int argc, char *argv[]) {
 	sec = new BlockchainSecLib(IPC_PATH, ETH_MY_ADDR, ETH_CONTRACT_ADDR); //TODO Load these from config file
 
-	cout << ".:Blockchain Security Framework Client:.\n";
+	cout << ".:Blockchain Security Framework Client:." << endl;
 
 #ifdef _DEBUG
 	sec->test();
 #endif //_DEBUG
+
+	cout << "Double 7: " << sec->contract_double(7) << endl;
+	cout << "Double 11: " << sec->contract_double(11) << endl;
+	cout << "Double 13: " << sec->contract_double(13) << endl;
 
 	return 0;
 }
--- client.h
+++ client.h
@@ -4,8 +4,8 @@
 
 #define IPC_PATH				"/home/kale/.ethereum/geth.ipc"
 
-#define ETH_MY_ADDR				""
-#define ETH_CONTRACT_ADDR		""
+#define ETH_MY_ADDR				"0xa4528ce8f47845b3bbf842da92bae9359e23fa3b"
+#define ETH_CONTRACT_ADDR		"0x3ceef731e66055f20ceb427432a98d387b12a1e1"
 
 
 #endif //__CLIENT_H
